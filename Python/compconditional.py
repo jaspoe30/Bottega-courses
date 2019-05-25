a0b70db0da48fe9f2a8ee3fcd2e3b559cc910f0e
jaspoe30 <49917825+jaspoe30@users.noreply.github.com>
2019-05-24 19:46:02 -0500

    Renamed file for compound conditional

--- Python/compconditional.py
+++ Python/compconditional.py
@@ -1,8 +1,8 @@
-username = 'jonsnow'
+username = ''
 email = 'jon@snow.com'
 password = 'thenorth'
 
-if username == 'jonnow' and password == 'thenorth':
+if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
     print('Access permitted')
 else:
     print('You shall not pass')
