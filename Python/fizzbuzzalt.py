22908a2ed8756851af88663743effbc63d502683
jaspoe30 <49917825+jaspoe30@users.noreply.github.com>
2019-05-26 11:28:20 -0500

    added alternate fizzbuzz

---
+++ Python/fizzbuzzalt.py
@@ -0,0 +1,12 @@
+def fizz_buzz(max_num):
+    for num in range(1, max_num + 1):
+        if num % 3 == 0:
+            print('Fizz')
+        if num % 5 == 0:
+            print('Buzz')
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print(num)
+
+fizz_buzz(76)
