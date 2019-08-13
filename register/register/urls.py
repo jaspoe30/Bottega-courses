096d8743b2368bd3b4dd5614bacaaad112896aee
jaspoe30 <49917825+jaspoe30@users.noreply.github.com>
2019-08-12 19:20:31 -0500

    removed register

--- register/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from register import views as v
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("register/", v.register, name="register"),  # <-- added
-    path('', include("register.urls")),
-]
